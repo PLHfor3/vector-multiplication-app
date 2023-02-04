a7f625ba98ba6498b1ec74c167a9fd239235ebd5
Preston Higginbotham <prestonhigg17@gmail.com>
2023-02-04 01:45:45 -0600

    Initial Commit

---
+++ andrew mac vectors app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  andrew mac vectors app
+//
+//  Created by Preston Higginbotham on 2/4/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ andrew mac vectors appUITests/andrew_mac_vectors_appUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  andrew_mac_vectors_appUITestsLaunchTests.swift
+//  andrew mac vectors appUITests
+//
+//  Created by Preston Higginbotham on 2/4/23.
+//
+
+import XCTest
+
+final class andrew_mac_vectors_appUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
