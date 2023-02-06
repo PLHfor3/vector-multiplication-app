217613bc60e54d7f9d3de3bbdb945c40dc99ac80
Preston Higginbotham <prestonhigg17@gmail.com>
2023-02-05 21:10:38 -0600

    Reading Lost View Controller?

---
+++ andrew mac vectors app/CrossProductController.swift
@@ -0,0 +1,17 @@
+//
+//  CrossProductController.swift
+//  andrew mac vectors app
+//
+//  Created by Preston Higginbotham on 2/5/23.
+//
+
+import UIKit
+
+class CrossProductController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+}
